c30d9bef08e652a1953af03e71621d15afebf4fd
JokkeIver <jc.ivers1@gmail.com>
2025-09-02 15:00:43 +0200

    [ADDED] Event file for storeing upcoming events

---
+++ src/lib/data/events.js
@@ -0,0 +1,28 @@
+//          ╭─────────────────────────────────────────────────────────╮
+//          │  To store all the data for upcoming events, and weekly  │
+//          │                         things.                         │
+//          │              It has the following things:               │
+//          │                            {                            │
+//          │                       title: "",                        │
+//          │                        date: "",                        │
+//          │                      location: "",                      │
+//          │                    description: "",                     │
+//          │                            }                            │
+//          ╰─────────────────────────────────────────────────────────╯
+//
+export const events = [
+  {
+    
+  }
+]
+
+export const weekly = [
+  {
+    title: "Weekly",
+    date: "Monday",
+    location: "Nygaard",
+    description: "We meet at 17:15pm in the intersection outside of Nygaard. The monday run is going to be switching between "
+
+  },
+
+]
