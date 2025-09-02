feb8e1ea212db6e902858c0add29cef3becda411
JokkeIver <jc.ivers1@gmail.com>
2025-09-02 21:07:56 +0200

    [UPDATE] Added the weekly runs in the event.js
    Added our weekly events in the event file to be importet to the calendar
    page

--- src/lib/data/events.js
+++ src/lib/data/events.js
@@ -21,8 +21,19 @@ export const weekly = [
     title: "Weekly",
     date: "Monday",
     location: "Nygaard",
-    description: "We meet at 17:15pm in the intersection outside of Nygaard. The monday run is going to be switching between "
-
+    description: "We meet at 17:15pm in the intersection outside of Nygaard. The monday run is going to be switching between different kinds of run from normal run, intervals and things we think would be fun - to check what type of run it is look at the event in strava."
   },
+  {
+    title: "Weekly",
+    date: "Thursday",
+    location: "Nygaard",
+    description: "We meet at 16:15 in the intersection outside of Nygaard. The thursday run is a simple run, often around 4km if not anything else have been decided, this run is made so everyone can join - from beginner runner to the runner who just finnished a half-marathon."
+  },
+  {
+    title: "RunTime x Ice Bathing",
+    date: "Check Strava",
+    location: "The Harbour Bath (Aarhus Ø)",
+    description: "At times we plan special events combining running and ice bathing. These take place on sundays in the winter months. This isn't a weekly occurence so you'll need to check the Strava group for specific dates. During the event we run a 2km route around Aarhus * before enjoying a refreshing dip in the cold harbour followed by a warm sauna."
+  }
 
 ]
