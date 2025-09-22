2b03f00f9d82ec92d4b14c2c44d543865ec10819
JokkeIver <jc.ivers1@gmail.com>
2025-09-22 13:21:37 +0200

    [STYLING] Done with styling

--- src/lib/components/upcommingEvents.svelte
+++ src/lib/components/upcommingEvents.svelte
@@ -5,26 +5,47 @@ let selected = 0;
 
 <div class="container">
   <h2>Upcoming Events</h2> 
-  <div class="eventDetail">
-    <p class="evenTitle">{events[selected].title}</p>
-    <p class="eventDate">{events[selected].date}</p>
-    <p class="eventLocation">{events[selected].location}</p>
-    <p class="moreInfo"><a href="/Calendar">More info</a></p>
-  </div>
-  <div class="indexDots">
-    {#each events as _, index}
-      <div class="dot {selected === index ? 'active' : '' }" on:click={() => selected = index}></div>
-    {/each}
-  </div>
+  {#if events.length > 0}
+    <div class="eventDetail">
+      <p class="evenTitle">{events[selected].title}</p>
+      <p class="eventDate">{events[selected].date}</p>
+      <p class="eventLocation">{events[selected].location}</p>
+      <p class="moreInfo"><a href="/Calendar">More info</a></p>
+    </div>
+    <div class="indexDots">
+      {#each events as _, index}
+        <div class="dot {selected === index ? 'active' : '' }" on:click={() => selected = index}></div>
+      {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
 .container {
-  border: 1px solid rebeccapurple;
+  display: flex;
+  flex-direction: column;
+  padding-top: 10px;
+  align-items: center;
+}
+
+
+.container h2 {
+  border-bottom: 1px solid var(--secondary-color);
+  width: fit-content;
 }
 
 .eventDetail {
-  border: 1px solid pink;
+  padding: 7px;
+}
+
+.eventDetail a {
+  text-decoration: none;
+  color: var(--secondary-color);
+}
+
+.eventDetail a:hover {
+  color: var(--secondary-color);
+  filter: brightness(170%);
 }
 
 .indexDots {
@@ -34,17 +55,17 @@ let selected = 0;
   gap: 7px;
 }
 .dot {
-  border: 1px solid green;
+  border: 1px solid var(--primary-color);
   border-radius: 50%;
   height: 10px;
   width: 10px;
 }
 .dot:hover {
-  background: green;
+  background: var(--primary-color);
   cursor: pointer;
 }
 
 .active {
-  background: green;
+  background: var(--primary-color);
 }
 </style>
